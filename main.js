c0895b1f48cda83c5bd25ad6373323ebacedc706
sangadiswathikumari25 <sangadiswathikumari25@gmail.com>
2026-06-09 12:10:05 -0700

    style.css,main.js,img.png

---
+++ main.js
@@ -0,0 +1,7 @@
+// Setup and start animation! 
+    var typed = new Typed('#element', {
+    strings: ['<b>Web Developer<b>.', '<b>Graphic Designer<b>.','<b>Data Analytics<b>.'],
+    typeSpeed: 50,
+    backSpeed:30,
+    loop:true
+});
